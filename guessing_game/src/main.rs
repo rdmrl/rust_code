408dafd23dc5b32d4d55fc7310a6fed4aea0d8b8
Ravi Damarla <ravi.damarla@gmail.com>
2022-02-15 17:31:57 +0000

    finished the game implementation

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     // println is a macro and not a function.
@@ -14,33 +15,57 @@ fn main() {
     // but exclusive on the upper bound.
     let secret_number = rand::thread_rng().gen_range(1..100);
 
-    println!("The secret number is {}", secret_number);
+    loop {
+        println!("Please input your guess.");
 
-    println!("Please input your guess.");
+        // Create a mutable variable that is currently bound
+        // to a new, empty instance of a String.
+        let mut guess = String::new();
 
-    // Create a mutable variable that is currently bound
-    // to a new, empty instance of a String.
-    let mut guess = String::new();
+        // stdin() returns an instance of the Stdin type which 
+        // represents a handle to the standard input.
 
-    // stdin() returns an instance of the Stdin type which 
-    // represents a handle to the standard input.
+        // read_line takes the user input and appends to the string
+        // The string argument needs to be mutable so that read_line
+        // can change its contents.
 
-    // read_line takes the user input and appends to the string
-    // The string argument needs to be mutable so that read_line
-    // can change its contents.
+        // The & indicates that this argument is a reference. This
+        // allows access to this data without needing copies.
 
-    // The & indicates that this argument is a reference. This
-    // allows access to this data without needing copies.
+        // read_line also returns a value - io::Result, an enumeration.
+        // If the Result instance is an Err value, expect will cause the
+        // program to crash and display this message.
 
-    // read_line also returns a value - io::Result, an enumeration.
-    // If the Result instance is an Err value, expect will cause the
-    // program to crash and display this message.
+        // Not using expect will generate a warning.
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line.");
 
-    // Not using expect will generate a warning.
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line.");
+        // Using a match to continue with the loop if there is invalid input.
+        // parse returns Ok if able to convert the input string to a number.
+        // parse returns Err if not able to parse the string.
+        // The underscore in Err() is a catchall value used to match all 
+        // Err values, effectively ignoring all errors.
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue, 
+        };
 
-    // {} is a placeholder
-    println!("You guessed {}", guess);
+        // {} is a placeholder
+        println!("You guessed {}", guess);
+
+        // Use another type cmp::Ordering for comparison.
+
+        // cmp compares two values and returns an Ordering enum.
+        // The match expression is used to decide what to do next.
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                // Break out of the loop.
+                break;
+            }
+        }
+    }
 }
