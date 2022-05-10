a26c2ec743d1dc475ac67ecb11ee94aed6f301b8
Ravi Damarla <ravi.damarla@gmail.com>
2022-05-10 16:35:27 +0000

    added method with more parameters

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -14,12 +14,24 @@ impl Rectangle {
   fn width(&self) -> bool {
     self.width > 0
   }
+
+  fn can_hold(&self, other: &Rectangle) -> bool {
+    self.width > other.width && self.height > other.height
+  }
 }
 
 fn main() {
   let rect1 = Rectangle {
     width: 30,
     height: 50,
+  };
+  let rect2 = Rectangle {
+    width: 10,
+    height: 40,
+  };
+  let rect3 = Rectangle {
+    width: 60,
+    height: 45,
   };
 
   println!(
@@ -30,6 +42,9 @@ fn main() {
   if rect1.width() {
     println!("The rectangle has a nonzero width; it is {}", rect1.width);
   }
+
+  println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+  println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
 
 /*
