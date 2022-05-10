fb8df71b9c7a250cce3d6113ca36711ce840ca6f
Ravi Damarla <ravi.damarla@gmail.com>
2022-05-10 15:48:45 +0000

    area using method syntax

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,3 +1,44 @@
+// With methods
+
+#[derive(Debug)]
+struct Rectangle {
+  width: u32,
+  height: u32,
+}
+
+impl Rectangle {
+  fn area(&self) -> u32 {
+    self.width * self.height
+  }
+
+  fn width(&self) -> bool {
+    self.width > 0
+  }
+}
+
+fn main() {
+  let rect1 = Rectangle {
+    width: 30,
+    height: 50,
+  };
+
+  println!(
+    "The area of the rectangle is {} square pixels.",
+    rect1.area()
+  );
+
+  if rect1.width() {
+    println!("The rectangle has a nonzero width; it is {}", rect1.width);
+  }
+}
+
+/*
+// The parameter is an immutable borrow for a struct Rectangle instance.
+// main retains the ownership of this instance and can continue using it.
+fn area(rectangle: &Rectangle) -> u32 {
+  rectangle.width * rectangle.height
+}
+
 // With structs
 
 #[derive(Debug)]
@@ -28,8 +69,6 @@ fn main() {
 fn area(rectangle: &Rectangle) -> u32 {
   rectangle.width * rectangle.height
 }
-
-/*
 
 // With tuples.
 fn main() {
