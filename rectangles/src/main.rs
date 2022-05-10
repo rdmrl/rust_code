4c2856ba2f57a75cbe504632de72619b78db3441
Ravi Damarla <ravi.damarla@gmail.com>
2022-05-10 14:36:41 +0000

    using dbg

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,22 +1,30 @@
 // With structs
 
+#[derive(Debug)]
 struct Rectangle {
   width: u32,
   height: u32,
 }
 
 fn main() {
+  let scale = 2;
   let rect1 = Rectangle {
-    width: 30,
+    width: dbg!(30 * scale),
     height: 50
   };
+
+  println!("rect1 is {:?}", rect1);
 
   println!(
     "The area of the rectangle is {} square pixels.",
     area(&rect1)
   );
+
+  dbg!(&rect1);
 }
 
+// The parameter is an immutable borrow for a struct Rectangle instance.
+// main retains the ownership of this instance and can continue using it.
 fn area(rectangle: &Rectangle) -> u32 {
   rectangle.width * rectangle.height
 }
