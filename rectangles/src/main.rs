69a3aa1be59c5bbba3a8364a49053b67af71afb6
Ravi Damarla <ravi.damarla@gmail.com>
2022-05-10 17:27:39 +0000

    added associated function

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -18,6 +18,13 @@ impl Rectangle {
   fn can_hold(&self, other: &Rectangle) -> bool {
     self.width > other.width && self.height > other.height
   }
+
+  fn square(size: u32) -> Rectangle {
+    Rectangle {
+      width: size,
+      height: size,
+    }
+  }
 }
 
 fn main() {
@@ -45,6 +52,10 @@ fn main() {
 
   println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
   println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+  let square_rect = Rectangle::square(3);
+
+  dbg!(&square_rect);
 }
 
 /*
