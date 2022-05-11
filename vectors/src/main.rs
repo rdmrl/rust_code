f2a4354b2a978813770b4bb49d85ac9e9c6a1e33
Ravi Damarla <ravi.damarla@gmail.com>
2022-05-11 20:47:54 +0000

    vectors with enums

--- vectors/src/main.rs
+++ vectors/src/main.rs
@@ -1,3 +1,9 @@
+enum SpreadsheetCell {
+    Int(i32),
+    Float(f64),
+    Text(String),
+}
+
 fn main() {
     let v = vec![1, 2, 3, 4, 5];
 
@@ -8,4 +14,21 @@ fn main() {
         Some(third) => println!("The third element is {}", third),
         None => println!("There is no third element."),
     }
+
+    let v = vec![100, 32, 57];
+    for i in &v {
+        println!("{}", i);
+    }
+
+    let mut v = vec![100, 32, 57];
+    for i in &mut v {
+        *i += 50;
+        println!("{}", i);
+    }
+
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.13),
+    ];
 }
