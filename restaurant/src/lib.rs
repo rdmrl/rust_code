9f717d20daa712c9345d0af997dd6cfd8867fe18
Ravi Damarla <ravi.damarla@gmail.com>
2022-05-11 20:24:40 +0000

    split into separate packages

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,34 +1,50 @@
-/*
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+// Load the contents of the module from another file
+// with the same name as the module.
+mod front_of_house;
+
+fn serve_order() {
+}
+
+mod back_of_house {
+  
+  pub struct Breakfast {
+    pub toast: String,
+    seasonal_fruit: String,
+  }
+
+  impl Breakfast {
+    pub fn summer(toast: &str) -> Breakfast {
+      Breakfast {
+        toast: String::from(toast),
+        seasonal_fruit: String::from("peaches"),
+      }
     }
-}
-*/
-
-mod front_of_house {
-  pub mod hosting {
-    pub fn add_to_waitlist() {}
-
-    fn seat_at_table() {}
   }
 
-  mod serving {
-    fn take_order() {}
-
-    fn serve_order() {}
-
-    fn take_payment() {}
+  fn fix_incorrect_order() {
+    cook_order();
+    super::serve_order();
   }
+
+  fn cook_order() {}
 }
+
+use crate::front_of_house::hosting;
 
 pub fn eat_at_restaurant() {
   // Absolute path
-  crate::front_of_house::hosting::add_to_waitlist();
+  hosting::add_to_waitlist();
 
   // Relative path
-  front_of_house::hosting::add_to_waitlist();
+  hosting::add_to_waitlist();
+
+  // Order a breakfast in the summer with Rye toast.
+  let mut meal = back_of_house::Breakfast::summer("Rye");
+
+  // Change our mind about the bread.
+  meal.toast = String::from("Wheat");
+
+  println!("I'd like {} toast please", meal.toast);
+
+  // meal.seasonal_fruit = String::from("blueberries");
 }
