4001a1da0e499d59086f4013d6d78ae4fa5bad3e
Ravi Damarla <ravi.damarla@gmail.com>
2022-05-13 18:13:55 +0000

    using error propagation

--- errors/src/main.rs
+++ errors/src/main.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::io::ErrorKind;
+use std::io::{self, Read, ErrorKind};
 
 fn main() {
     /*
@@ -19,6 +19,7 @@ fn main() {
     };
     */
 
+    /*
     // With closures.
     let f = File::open("hello.txt").unwrap_or_else(|error| {
         if error.kind() == ErrorKind::NotFound {
@@ -29,4 +30,24 @@ fn main() {
             panic!("Problem opening the file: {:?}", error)
         }
     });
+    */
+
+    let res = read_username_from_file();
+    println!("Result: {:?}", res);
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let f = File::open("hello.txt");
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
 }
