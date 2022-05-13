d182c17542b2ca13730eef9100340fb65ee3a7ab
Ravi Damarla <ravi.damarla@gmail.com>
2022-05-13 18:15:19 +0000

    shortcut for propagating errors

--- errors/src/main.rs
+++ errors/src/main.rs
@@ -37,6 +37,7 @@ fn main() {
 }
 
 fn read_username_from_file() -> Result<String, io::Error> {
+/*
     let f = File::open("hello.txt");
 
     let mut f = match f {
@@ -50,4 +51,9 @@ fn read_username_from_file() -> Result<String, io::Error> {
         Ok(_) => Ok(s),
         Err(e) => Err(e),
     }
+*/
+    let mut f = File::open("hello.txt")?;
+    let mut s = String::new();
+    f.read_to_string(&mut s)?;
+    Ok(s)
 }
