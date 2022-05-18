9612141fb1f453c45b1403370328b509d09ce6c3
Ravi Damarla <ravi.damarla@gmail.com>
2022-05-18 19:34:10 +0000

    added test and implemented method to search lines

---
+++ minigrep/src/lib.rs
@@ -0,0 +1,71 @@
+use std::error::Error;
+use std::fs;
+
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        // Using clone to allow the Config instance to own
+        // these values.
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
+}
+
+// Box<dyn Error> is a trait object. This function will return
+// a type that implements the Error trait but the particular type
+// need not be specified. dyn is short for dynamic.
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+    // expect has been replaced with a ? to avoid panic!.
+    // Now it will return the error value from the current function
+    // for the caller to handle.
+
+    // println!("With text:\n{}", contents);
+
+    // Returns an OK value in the success case. The success type is ().
+    Ok(())
+}
+
+// Need an explicit lifetime 'a defined in the signature of search.
+// The returned vector should contain string slices that reference
+// slices of the argument contents (rather than the argument) query.
+
+// The data returned by the search function will live as long as the
+// data passed into the search function in the contents argument.
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
+
