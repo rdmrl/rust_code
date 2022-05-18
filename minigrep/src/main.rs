7b2f2847da6f7485880fa60ff9c949a16d6eb543
Ravi Damarla <ravi.damarla@gmail.com>
2022-05-18 19:34:24 +0000

    moved code into src/lib.rs

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
-use std::fs;
 use std::process;
-use std::error::Error;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -17,39 +17,11 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}\n", config.filename);
 
-    run(config);
-}
-
-// Box<dyn Error> is a trait object. This function will return
-// a type that implements the Error trait but the particular type
-// need not be specified. dyn is short for dynamic.
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
-    // expect has been replaced with a ? to avoid panic!.
-    // Now it will return the error value from the current function
-    // for the caller to handle.
-
-    println!("With text:\n{}", contents);
-
-    // Returns an OK value in the success case. The success type is ().
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        // Using clone to allow the Config instance to own
-        // these values.
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
+    // run returns an () in the success case.
+    // Only need to detect an error so unwrap_or_else is not needed.
+    // as with Config::new.
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
     }
 }
